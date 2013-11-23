4de49039178a08747d9810eb88bcc61c044bb0e2
Jon Gjerset <jon@gjerset.com>
2013-11-23 13:40:56 -0800

    Tests complete and alert messagerefinement

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,14 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :profile_name
 
-  validates :first_name, :last_name, :profile_name, presence: true, 
-  													uniqueness: true
+  validates :first_name, :last_name, presence: true
+
+  validates :profile_name, presence: true, 
+                           uniqueness: true,
+                           format: {
+                              with: /a-zA-Z0-1_-/,
+                              message: 'Profile name must not contain spaces'
+                           }
 
   has_many :statuses
   
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,6 +7,40 @@ class UserTest < ActiveSupport::TestCase
 		assert !user.errors[:first_name].empty?
 	end
 
+	test "a user should enter a last name"  do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:last_name].empty?
+	end
+
+#	test "a user should enter a profile name"  do
+#		user = User.new
+#		assert !user.save
+#		assert !user.errors[:profile_name].empty?
+#	end
+
+	test "a user should enter an email address"  do
+		user = User.new
+		assert !user.save
+		assert !user.errors[:email].empty?
+	end
+
+	test "a user should enter a unique profile"  do
+		user = User.new
+		user.profile_name = users(:jon).profile_name
+		assert !user.save
+		#puts user.errors.inspect
+		assert !user.errors[:profile_name].empty?
+	end
+
+	test "a profile name should not contain spaces" do
+		user = User.new
+		user.profile_name = "Name with spaces"
+
+		assert !user.save
+		assert !user.errors[:profile_name].empty?
+		assert user.errors[:profile_name].include?("Profile name must not contain spaces")
+	end
 
 
 end
